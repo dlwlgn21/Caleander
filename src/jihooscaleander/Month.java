eb2ec74e7053c0a3d3f7827e13e8afcd965470ae
이종만 <@10.0.0.20>
2017-10-23 09:18:36 +1000

    I understand.

--- src/jihooscaleander/Month.java
+++ src/jihooscaleander/Month.java
@@ -6,21 +6,30 @@ public class Month {
 	int[] a = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int maxdayofMonth(int num) {
-		return a[num - 1];	
+		return a[num - 1];
 	}
 
 	public static void main(String[] args) {
 		Month mon = new Month();
-		// int [] a = {31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.println("달을 입력해주세요. : ");
-		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		if (12<num) { 
-			System.out.println("잘못된 값을 입력하셨습니다");
-		}
-		
-		System.out.printf("%d월은 %d일 까지 있습니다. \n", num, mon.maxdayofMonth(num));
-		scanner.close();
+		String PROMPT = " CAL> ";
 
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("달을 입력해주세요.");
+			System.out.print(PROMPT);
+			int num = scanner.nextInt();
+			if (num == -1) {
+				System.out.println("안녕~~");
+				break;
+			} else if (num > 12) {
+				System.out.println("바이바이");
+				break;
+			}
+			System.out.printf("%d월은 %d일 까지 있습니다. \n", num, mon.maxdayofMonth(num));
+
+		}
+		 scanner.close();	
 	}
+
 }
